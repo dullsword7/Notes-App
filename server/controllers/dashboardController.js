b846b4664cec2f2de5835ac3c59b299c0e81b794
Alan Wu <57293716+dullsword7@users.noreply.github.com>
2023-09-29 03:38:14 -0400

    Search Functionality Complete

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -87,7 +87,7 @@ exports.dashboardUpdateNote= async(req, res) => {
   try {
     await Note.findOneAndUpdate(
       { _id: req.params.id },
-      { title: req.body.title, body: req.body.body }
+      { title: req.body.title, body: req.body.body, updatedAt: Date.now() }
     ).where( { user: req.user.id } );
     res.redirect('/dashboard');
 
@@ -131,6 +131,48 @@ exports.dashboardAddNoteSubmit = async(req, res) => {
     req.body.user = req.user.id;
     await Note.create(req.body);
     res.redirect('/dashboard');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * GET /
+ * Search
+ */
+
+exports.dashboardSearch = async(req, res) => {
+  try {
+    res.render('dashboard/search', {
+      searchResults: '',
+      layout: '../views/layouts/dashboard'
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/**
+ * POST /
+ * Search for notes
+ */
+
+exports.dashboardSearchSubmit = async(req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+    const searchResults = await Note.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChars, 'i')}},
+        { body: { $regex: new RegExp(searchNoSpecialChars, 'i')}}
+      ]
+    }).where({ user: req.user.id });
+
+    res.render('dashboard/search', {
+      searchResults,
+      layout: '../views/layouts/dashboard'
+    })
   } catch (error) {
     console.log(error);
   }
