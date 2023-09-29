9055360203dfcabf059d961ed05ccf12a5900183
Alan Wu <57293716+dullsword7@users.noreply.github.com>
2023-09-29 00:27:42 -0400

    Delete Note Complete

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -9,6 +9,7 @@ const dashboardController = require('../controllers/dashboardController');
 
 router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
 router.get('/dashboard/item/:id', isLoggedIn, dashboardController.dashboardViewNote);
-router.post('/dashboard/item/:id', isLoggedIn, dashboardController.dashboardUpdateNote);
+router.put('/dashboard/item/:id', isLoggedIn, dashboardController.dashboardUpdateNote);
+router.delete('/dashboard/item-delete/:id', isLoggedIn, dashboardController.dashboardDeleteNote);
 
 module.exports = router;
